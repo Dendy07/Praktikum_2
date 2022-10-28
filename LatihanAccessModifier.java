75970ff81faa802d5f4c1bc94c118acb1c8aae32
Dendy07 <dendyscik@gmal.com>
2022-10-28 16:38:56 +0700

    Menambahkan file LatihanAccessModifier.java

---
+++ LatihanAccessModifier.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.latihanaccessmodifier;
+
+/**
+ *
+ * @author Aspire-ES14
+ */
+public class LatihanAccessModifier {
+
+    public static void main(String[] args) {
+        System.out.println("Person");
+        System.out.println("- nama");
+        System.out.println("- jeniskelamin");
+        System.out.println("- umur\n");
+        
+        System.out.println("+ setnama()");
+        System.out.println("+ getnama()");
+        System.out.println("+ setjeniskelamin()");
+        System.out.println("+ getjenikelamin()");
+        System.out.println("+ setUmur()");
+        System.out.println("+ setUmur()");
+    }
+}
